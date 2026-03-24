b8a9f4d2543abcef2e541fe089114b2431917e46
frankchan <thefrankchan@gmail.com>
2026-03-24 08:51:00 -0700

    Remove Docusaurus cache from tracking

--- web/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/frankchan/dev/oss/proposals/kit/web/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/frankchan/dev/oss/proposals/kit/web/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/frankchan/dev/oss/proposals/kit/web/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/frankchan/dev/oss/proposals/kit/web/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/frankchan/dev/oss/proposals/kit/web/src/css/custom.css"),
-];
